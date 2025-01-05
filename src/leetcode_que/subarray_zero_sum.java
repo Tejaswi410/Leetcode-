5513f0ed8333925ff6d9baac4ae177d99f647585
Tejaswi Yadav <tejaswi40yadav@gmail.com>
2025-01-05 21:25:53 +0530

    find the longest subarray with sum zero

---
+++ src/leetcode_que/subarray_zero_sum.java
@@ -0,0 +1,32 @@
+package leetcode_que;
+import java.util.*;
+
+
+public class subarray_zero_sum {
+    public static void main(String[] args) {
+      int[] arr = {1, 2, -2, 4, -4};
+      subarray_zero_sum(arr);
+    }
+
+    public static int subarray_zero_sum(int[] arr){
+        int n = arr.length;
+        HashMap<Integer,Integer> mpp = new HashMap<>();
+        int max=0;
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+= arr[i];
+            if(sum==0){
+                max= i+1;
+            }
+            else{
+                if(mpp.get(sum)!=null){
+                    max=Math.max(max,i - mpp.get(sum));
+                }
+                else{
+                    mpp.put(sum,i);
+                }
+            }
+        }
+        return max;
+    }
+}
