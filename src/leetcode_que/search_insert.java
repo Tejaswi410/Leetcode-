fe546de69342fcc77a5d4239c455345d8567044e
Tejaswi Yadav <tejaswi40yadav@gmail.com>
2025-01-06 23:07:24 +0530

    search insert using binary search

---
+++ src/leetcode_que/search_insert.java
@@ -0,0 +1,22 @@
+package leetcode_que;
+//https://leetcode.com/problems/search-insert-position
+
+public class search_insert {
+    public int searchInsert(int[] nums, int target) {
+        int n = nums.length;
+        int low = 0;
+        int high = n-1;
+        int ans = n;
+        while(low <= high){
+            int mid = (low + high)/2;
+            if(nums[mid]>=target){
+                ans = mid;
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+        return ans;
+    }
+}
