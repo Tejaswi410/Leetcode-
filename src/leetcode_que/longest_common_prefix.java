98bb745b0b914866411dc5cffc902fa93701f3f9
Tejaswi Yadav <tejaswi40yadav@gmail.com>
2025-01-25 19:18:47 +0530

    find the longest common prefix from the arr of strings

---
+++ src/leetcode_que/longest_common_prefix.java
@@ -0,0 +1,22 @@
+package leetcode_que;
+import java.util.*;
+//https://leetcode.com/problems/longest-common-prefix
+
+public class longest_common_prefix {
+    public String longestCommonPrefix(String[] strs) {
+        Arrays.sort(strs);
+        String str1=strs[0];
+        String str2=strs[strs.length-1];
+        int i=0;
+        while(i<str1.length() && i<str2.length()){
+            if(str1.charAt(i)==str2.charAt(i)){
+                i++;
+            }
+            else{
+                break;
+            }
+        }
+        return str1.substring(0,i);
+
+    }
+}
