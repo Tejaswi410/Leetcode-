ddbbf670e04022d1d0b36d64fd1023b081ef2d7c
Tejaswi Yadav <tejaswi40yadav@gmail.com>
2025-02-02 21:02:24 +0530

    check if array is sorted and rotated

---
+++ src/daily_que/two_feb.java
@@ -0,0 +1,19 @@
+package daily_que;
+//https://leetcode.com/problems/check-if-array-is-sorted-and-rotated?envType=daily-question&envId=2025-02-02
+
+public class two_feb {
+    public boolean check(int[] nums) {
+        int n = nums.length;
+        int cnt = 0;
+        for(int i=0; i<n-1; i++){
+            if(nums[i] > nums[(i+1)]){
+                cnt++;
+            }
+        }
+        if(nums[n-1] > nums[0]) cnt++;
+        if(cnt <= 1) return true;
+        else{
+            return false;
+        }
+    }
+}
