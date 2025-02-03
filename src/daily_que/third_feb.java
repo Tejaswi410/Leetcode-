700204366cb4019531abc2989a3ac614dc9b9b46
Tejaswi Yadav <tejaswi40yadav@gmail.com>
2025-02-03 23:28:33 +0530

    longest strictly increasing or strictly decreasing subarray

---
+++ src/daily_que/third_feb.java
@@ -0,0 +1,28 @@
+package daily_que;
+import java.util.*;
+//https://leetcode.com/problems/longest-strictly-increasing-or-strictly-decreasing-subarray?envType=daily-question&envId=2025-02-03
+
+public class third_feb {
+    public int longestMonotonicSubarray(int[] nums) {
+        int n = nums.length;
+        int increasing = 1;
+        int decreasing = 1;
+        int longest = 1;
+        for(int i=0; i<n-1 ; i++){
+            if(nums[i] < nums[i+1]){
+                increasing++;
+                decreasing = 1;
+            }
+            else if(nums[i] > nums[i+1]){
+                decreasing++;
+                increasing = 1;
+            }
+            else{
+                increasing = 1;
+                decreasing = 1;
+            }
+            longest = Math.max(longest,Math.max(increasing,decreasing));
+        }
+        return longest;
+    }
+}
