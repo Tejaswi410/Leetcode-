e4002d35abf6dc51cd883d48a72e4ac911f6d3de
Tejaswi Yadav <tejaswi40yadav@gmail.com>
2025-02-10 20:09:50 +0530

    count number of bad pairs

---
+++ src/daily_que/nineth_feb.java
@@ -0,0 +1,17 @@
+package daily_que;
+import java.util.*;
+//https://leetcode.com/problems/count-number-of-bad-pairs?envType=daily-question&envId=2025-02-10
+
+public class nineth_feb {
+    public long countBadPairs(int[] nums) {
+        Map<Integer, Integer> freqMap = new HashMap<>();
+        long goodpair = 0;
+        long totalPair = (long) nums.length * (nums.length - 1) / 2;
+        for (int i = 0; i < nums.length; i++) {
+            int key = nums[i] - i;
+            goodpair += freqMap.getOrDefault(key, 0);
+            freqMap.put(key, freqMap.getOrDefault(key, 0) + 1);
+        }
+        return totalPair - goodpair;
+    }
+}
