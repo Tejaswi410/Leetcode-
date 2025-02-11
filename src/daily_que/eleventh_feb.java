214a04c7da33856e72545f5e2a2605b9f85c4bf2
Tejaswi Yadav <tejaswi40yadav@gmail.com>
2025-02-11 23:16:06 +0530

    remove all occurences of a substring

---
+++ src/daily_que/eleventh_feb.java
@@ -0,0 +1,12 @@
+package daily_que;
+//https://leetcode.com/problems/remove-all-occurrences-of-a-substring?envType=daily-question&envId=2025-02-11
+
+
+public class eleventh_feb {
+    public String removeOccurrences(String s, String part) {
+        while(s.contains(part)){
+            s = s.replaceFirst(part,"");
+        }
+        return s;
+    }
+}
