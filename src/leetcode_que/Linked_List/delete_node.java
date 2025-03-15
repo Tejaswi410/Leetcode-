7ee9e032ec3ce2c8c25430196f1665f3b1162a92
Tejaswi Yadav <tejaswi40yadav@gmail.com>
2025-03-15 23:46:21 +0530

    delete a node from LL

---
+++ src/leetcode_que/Linked_List/delete_node.java
@@ -0,0 +1,19 @@
+package leetcode_que.Linked_List;
+import java.util.*;
+//https://leetcode.com/problems/delete-node-in-a-linked-list?envType=problem-list-v2&envId=linked-list
+
+
+  class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+  }
+
+public class delete_node {
+    class Solution {
+        public void deleteNode(ListNode node) {
+            node.val = node.next.val;
+            node.next = node.next.next;
+        }
+    }
+}
