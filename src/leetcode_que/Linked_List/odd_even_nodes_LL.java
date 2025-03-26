1fee6c2e17019aac3cefd1f2304c5e8e643f5367
Tejaswi Yadav <tejaswi40yadav@gmail.com>
2025-03-26 17:55:16 +0530

    segregate odd and even nodes in singly linked list

---
+++ src/leetcode_que/Linked_List/odd_even_nodes_LL.java
@@ -0,0 +1,19 @@
+package leetcode_que.Linked_List;
+//https://leetcode.com/problems/odd-even-linked-list
+public class odd_even_nodes_LL {
+    public ListNode oddEvenList(ListNode head) {
+        if(head == null || head.next == null) return head;
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenHead = head.next;
+        while(even != null && even.next != null){
+            odd.next = even.next;
+            odd = odd.next;
+
+            even.next = odd.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+        return head;
+    }
+}
