f14da3b41861aa9f569d5f92fafba555867b6c86
Tejaswi Yadav <tejaswi40yadav@gmail.com>
2025-04-06 22:03:52 +0530

    remove nodes of singly linked list by val

---
+++ src/leetcode_que/Linked_List/remove_node_byVal.java
@@ -0,0 +1,24 @@
+package leetcode_que.Linked_List;
+//https://leetcode.com/problems/remove-linked-list-elements?envType=problem-list-v2&envId=linked-list
+//using dummy node and 2 pointer i.e. prev and curr prev will point to dummy and dummy is connected to head of the LL
+//we will check if curr.val is equals to given val if it does then prev.next will point to curr.next and curr will point to null.
+
+public class remove_node_byVal {
+    public ListNode removeElements(ListNode head, int val) {
+        if(head == null) return head;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode curr = head;
+        ListNode prev = dummy;
+        while(curr != null){
+            if(curr.val == val){
+                prev.next = curr.next;
+            }
+            else{
+                prev = curr;
+            }
+            curr = curr.next;
+        }
+        return dummy.next;
+    }
+}
